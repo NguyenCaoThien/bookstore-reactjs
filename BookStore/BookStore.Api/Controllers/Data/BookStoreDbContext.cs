781b411d2c53309edbe57dfe72125b059294c5d0
Nguyen Cao Thien <nguyenthien250797@gmail.com>
2022-06-30 22:44:43 +0700

    Connect Sqlite and Insert Product  data

---
+++ BookStore/BookStore.Api/Controllers/Data/BookStoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Api.Controllers.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Api.Controllers.Data
+{
+    public class BookStoreDbContext: DbContext
+    {
+        public BookStoreDbContext(DbContextOptions options): base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
