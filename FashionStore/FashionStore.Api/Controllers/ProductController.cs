cf608a240370c234b8bd549a785b921cdb2db4c6
Nguyen Cao Thien <nguyenthien250797@gmail.com>
2022-07-09 18:10:42 +0700

    Rename project

---
+++ FashionStore/FashionStore.Api/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using FashionStore.Api.Controllers.Data;
+using FashionStore.Api.Controllers.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FashionStore.Api.Controllers
+{
+    [ApiController]
+    [Route("api/product")]
+    public class ProductController : Controller
+    {
+        private readonly FashionStoreDbContext _storeDbContext;
+        public ProductController(FashionStoreDbContext storeDbContext)
+        {
+            this._storeDbContext = storeDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetAlls()
+        {
+            var products = await this._storeDbContext.Products.ToListAsync();
+            return Ok(products);
+        }       
+
+        [HttpGet("id")]
+        public async Task<ActionResult<Product>> GetById(int id)
+        {
+            var product = await this._storeDbContext.Products.FindAsync(id);
+            return Ok(product);
+        }
+
+    }
+}
---
+++ FashionStore/FashionStore.Api/Data/FashionStoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FashionStore.Api.Controllers.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FashionStore.Api.Controllers.Data
+{
+    public class FashionStoreDbContext: DbContext
+    {
+        public FashionStoreDbContext(DbContextOptions options): base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
